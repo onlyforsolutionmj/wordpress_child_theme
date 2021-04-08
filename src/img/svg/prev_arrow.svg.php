6d89b615935d97ad2b7aa53c369de6481a274d24
Dan Groch <daniel.j.groch@gmail.com>
2021-04-08 15:27:11 +1000

    add sass and mix to project

---
+++ src/img/svg/prev_arrow.svg.php
@@ -0,0 +1,5 @@
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 width="16.264px" height="31.264px" viewBox="0 0 16.264 31.264" enable-background="new 0 0 16.264 31.264" xml:space="preserve">
+	<polygon fill-rule="evenodd" clip-rule="evenodd" points="0,15.557 15.557,0 16.264,0.707 1.339,15.632 16.264,30.557 
+			15.557,31.264 0,15.707 0.075,15.632 		"/>
+</svg>
