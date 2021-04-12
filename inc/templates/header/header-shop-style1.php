75335d916e87275db24c5a3cd3a7fe8bd74e15f2
Dan Groch <daniel.j.groch@gmail.com>
2021-04-12 23:44:08 +1000

    fix styling of page title

---
+++ inc/templates/header/header-shop-style1.php
@@ -0,0 +1,8 @@
+<div class="row row-fluid full-width-row">
+    <div class="small-12 columns">
+      <div class="small 12 columns">
+        <h1 style="margin: 0.5rem 0 1 rem 0;" class="thb-shop-title"><?php woocommerce_page_title(); ?></h1>
+      </div>
+    </div>
+  </div>
+</div>
